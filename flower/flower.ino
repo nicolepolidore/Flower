01bf9e9ea3fb1fc48a2c4c466297d5872f832816
Nicole <polidorenn@gmail.com>
2020-07-01 23:03:10 -0400

    bugs appear and disappear

--- flower/flower.ino
+++ flower/flower.ino
@@ -1,25 +1,30 @@
 #define TIMER_LENGTH 2000
-enum signalStates {PETAL,  CENTER};
-byte signalState = PETAL;
+#define BUG_DURATION 1200 //how long bugs will be there
+enum gameStates {PETAL,  CENTER, BUG};
+byte gameState = PETAL;
 
 
-Timer centerTimer;
+Timer bugTimer;
 
 bool isPetal = false;
 bool isCenter = false;
+bool isAttacked = false;
 
 void setup(){
-  centerTimer.set(TIMER_LENGTH);
 }
 
 void loop(){
 
-   switch(signalState){
+   switch(gameState){
     
-  case PETAL: //petal is really game state
+  case PETAL: 
     petalLoop();
     break;
-
+    
+  case BUG:
+  bugLoop();
+  break;
+  
   case CENTER:
    centerLoop();
    break;
@@ -27,49 +32,61 @@ void loop(){
   
   displaySignalState();
 
-  setValueSentOnAllFaces(signalState);
+  setValueSentOnAllFaces(gameState);
 }
 
 void petalLoop(){
-  if(buttonDoubleClicked()){
-    isPetal = false;
-    signalState = CENTER;
+  if(buttonDoubleClicked()){ //setting the center to yellow
+    gameState = CENTER;
   }
-  if(buttonDown()){
-    isPetal = true;
+  if(buttonDown()){ //reset the bug timer and bring it to petal gamestate
+    bugTimer.set(TIMER_LENGTH);
+    gameState = PETAL;
   }
+  if(bugTimer.isExpired()){ 
+    bugTimer.set(BUG_DURATION);
+    isAttacked = true;
+    gameState = BUG;
+  }
+  
  
   }
 
+void bugLoop(){
+
+  if(isAttacked){ //attacking petal
+    setColorOnFace(RED,1);
+    if(buttonDown()){
+      gameState = PETAL;
+    }
+  }
+  return;
+  
+  //here, I want to add petal health. 
+  //once a petal dies, player must remove the blink
+  //regrowth option? set aside a "butterfly" blink to regrow a petal. butterfly loses one health
+
+  //I definetely want to add "stronger" bugs, meaning you have to click them 3 times in order to kill them
+  
+}
 
 void centerLoop(){
-  int timeProgress = millis() % TIMER_LENGTH;
+  isCenter=true;
+  gameState=CENTER;
   
-  if(!centerTimer.isExpired()){ //if timer is expired
-    int timeProgress = centerTimer.getRemaining();
-  }
-      setColorOnFace(RED,1);
-
-  if(buttonDown()){
-    setColor(CYAN);
-  }
-//  FOREACH_FACE(f){
-//      if(!isValueReceivedOnFaceExpired(f)){
-//
-//        byte neighbor = getLastValueReceivedOnFace(f);
-//
-//        if(neighbor == buttonDown()){
-//          setColor(CYAN);
-//        }
-    }
-
-
+ 
+}
 
 void displaySignalState(){
-  switch(signalState){
+  switch(gameState){
     case PETAL:
     petalLoop();
     setColor(CYAN);
+    break;
+
+    case BUG:
+    bugLoop();
+    setColorOnFace(RED,1);
     break;
 
    case CENTER:
