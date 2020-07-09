17964313671698a3914dfd8fe469b036df5cb149
Nicole <polidorenn@gmail.com>
2020-07-09 18:18:05 -0400

    bugs are buggy

--- flower/flower.ino
+++ flower/flower.ino
@@ -1,14 +1,25 @@
 #define TIMER_LENGTH 2000
+#define TIMER_LENGTH_TWO 6000
 #define BUG_DURATION 1200 //how long bugs will be there
-enum gameStates {PETAL,  CENTER, BUG};
+#define BUG_DURATION_TWO 2500
+enum gameStates {PETAL,  CENTER, BUG, BUG2};
 byte gameState = PETAL;
 
 
 Timer bugTimer;
+Timer bugTimer2;
 
 bool isPetal = false;
 bool isCenter = false;
 bool isAttacked = false;
+bool isAttackedBig = false;
+bool longPressing = false;
+
+//byte bugLvl_One;
+//byte bugLvl_Two;
+//byte bugLvl_Three;
+
+byte clicks = 1;
 
 void setup(){
 }
@@ -24,6 +35,10 @@ void loop(){
   case BUG:
   bugLoop();
   break;
+
+  case BUG2:
+  bugTwoLoop();
+  break;
   
   case CENTER:
    centerLoop();
@@ -36,11 +51,18 @@ void loop(){
 }
 
 void petalLoop(){
-  if(buttonDoubleClicked()){ //setting the center to yellow
+  if(buttonLongPressed()){ //setting the center to yellow
+    longPressing = true;
+  
     gameState = CENTER;
   }
-  if(buttonDown()){ //reset the bug timer and bring it to petal gamestate
+  if(buttonDown()|| buttonDoubleClicked()){ //reset the bug timer and bring it to petal gamestate
     bugTimer.set(TIMER_LENGTH);
+    gameState = PETAL;
+  }
+
+  if(buttonDoubleClicked()){
+    bugTimer2.set(TIMER_LENGTH_TWO);
     gameState = PETAL;
   }
   if(bugTimer.isExpired()){ 
@@ -48,6 +70,13 @@ void petalLoop(){
     isAttacked = true;
     gameState = BUG;
   }
+
+  if(bugTimer2.isExpired()){
+    bugTimer2.set(BUG_DURATION_TWO);
+    isAttackedBig = true;
+    gameState = BUG2;
+  }
+ 
   
  
   }
@@ -56,7 +85,8 @@ void bugLoop(){
 
   if(isAttacked){ //attacking petal
     setColorOnFace(RED,1);
-    if(buttonDown()){
+    if(buttonSingleClicked()){
+      clicks = 1;
       gameState = PETAL;
     }
   }
@@ -70,8 +100,20 @@ void bugLoop(){
   
 }
 
+void bugTwoLoop(){
+  if(isAttacked){ //attacking petal
+    setColorOnFace(GREEN,4);
+    setColorOnFace(GREEN,3);
+    if(buttonDoubleClicked()){
+       clicks =2;
+       gameState = PETAL;
+    }
+   }
+}
+
 void centerLoop(){
   isCenter=true;
+  setColor(YELLOW);
   gameState=CENTER;
   
  
@@ -87,6 +129,12 @@ void displaySignalState(){
     case BUG:
     bugLoop();
     setColorOnFace(RED,1);
+    break;
+    
+    case BUG2:
+    bugTwoLoop();
+    setColorOnFace(GREEN,4);
+    setColorOnFace(GREEN,3);
     break;
 
    case CENTER:
